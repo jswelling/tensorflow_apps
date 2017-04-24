9558533e4e7326ff84061590b4392376cf8f1fa2
Richard Zhao <richardz@andrew.cmu.edu>
2017-04-24 01:28:47 -0400

    More consistent command line arguments

--- src/cnn/train.py
+++ src/cnn/train.py
@@ -32,13 +32,13 @@ import topology
 # Basic model parameters as external flags.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_string('network_pattern', 'whole_ball_1_hidden',
+flags.DEFINE_string('network_pattern', 'outer_layer_cnn',
                    'A network pattern recognized in topology.py')
 flags.DEFINE_float('learning_rate', 0.01,
                    'Initial learning rate.')
 flags.DEFINE_integer('batch_size', 12, 'Batch size.  '
                      'Must divide evenly into the dataset sizes.')
-flags.DEFINE_string('train_dir', '/home/welling/data/fish_cubes',
+flags.DEFINE_string('data_dir', '/home/welling/data/fish_cubes',
                     'Directory to put the training data.')
 flags.DEFINE_string('log_dir', '/home/welling/data/fish_logs',
                     'Directory to put logs and checkpoints.')
@@ -50,7 +50,7 @@ flags.DEFINE_integer('read_threads', 2,
                      'Number of threads reading input files')
 flags.DEFINE_integer('shuffle_size', 12,
                      'Number of input data pairs to shuffle (min_dequeue)')
-flags.DEFINE_integer('n_training_examples', 12,
+flags.DEFINE_integer('num_examples', 12,
                      'Number of examples used in a training epoch')
 flags.DEFINE_string('starting_snapshot', '',
                     'Snapshot from the end of the previous run ("" for none)')
@@ -68,13 +68,14 @@ def train():
     # Tell TensorFlow that the model will be built into the default Graph.
     with tf.Graph().as_default():
         # Generate placeholders for the images and labels.
-        images, labels = input_data.input_pipeline(FLAGS.train_dir,
-                                                             FLAGS.batch_size,
-                                                             fake_data=FLAGS.fake_data,
-                                                             num_epochs=num_epochs,
-                                                             read_threads=FLAGS.read_threads,
-                                                             shuffle_size=FLAGS.shuffle_size,
-                                                             num_expected_examples=FLAGS.n_training_examples)
+        images, labels = input_data.input_pipeline(FLAGS.data_dir,
+                                                   FLAGS.batch_size,
+                                                   fake_data=FLAGS.fake_data,
+                                                   num_epochs=num_epochs,
+                                                   read_threads=FLAGS.read_threads,
+                                                   shuffle_size=FLAGS.shuffle_size,
+                                                   num_expected_examples=FLAGS.num_examples)
+
         # Build a Graph that computes predictions from the inference model.
         logits = topology.inference(images, FLAGS.network_pattern)
 
@@ -83,9 +84,6 @@ def train():
 
         # Add to the Graph the Ops that calculate and apply gradients.
         train_op = topology.training(loss, FLAGS.learning_rate)
-
-        # Add the Op to compare the logits to the labels during evaluation.
-        eval_correct = topology.evaluation(logits, labels)
 
         # Build the summary operation based on the TF collection of Summaries.
         summary_op = tf.summary.merge_all()
@@ -126,14 +124,9 @@ def train():
                 start_time = time.time()
                 _, loss_value = sess.run([train_op, loss])
                 duration = time.time() - start_time
-#                 with sess.as_default():
-#                     print(featureBatch.eval())
-#                     print(labelBatch.eval())
 
                 # Write the summaries and print an overview fairly often.
-                if ((step + 1) % 100 == 0
-                    or step < 10
-                    ):
+                if ((step + 1) % 100 == 0 or step < 10):
                     # Print status to stdout.
                     print('Step %d: numerics = %s, loss = %.2f (%.3f sec)'
                           % (step, num_chk, loss_value, duration))
@@ -141,6 +134,10 @@ def train():
                     summary_str = sess.run(summary_op)
                     summary_writer.add_summary(summary_str, step)
                     summary_writer.flush()
+
+                # Save a checkpoint periodically.
+                if (step + 1) % 1000 == 0:
+                    saver.save(sess, FLAGS.log_dir, global_step=step)
 
                 step += 1
 
@@ -159,11 +156,11 @@ def train():
         coord.join(threads, stop_grace_period=10)
         sess.close()
 
-
-
 def main(_):
+    if tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.DeleteRecursively(FLAGS.log_dir)
+    tf.gfile.MakeDirs(FLAGS.log_dir)
     train()
-
 
 if __name__ == '__main__':
     tf.app.run()
