75fb5c607d62542dab1c504535fd3ecd84ad0ca5
Joel Welling <welling@psc.edu>
2019-06-20 22:52:14 -0400

    Some restructuring of topology module.  added train_binaryclassifier.

--- src/cnn/topology.py
+++ src/cnn/topology.py
@@ -38,6 +38,8 @@ from tensorflow.python.framework import dtypes
 
 from input_data import N_BALL_SAMPS, OUTERMOST_SPHERE_SHAPE
 
+OUTERMOST_SPHERE_N = OUTERMOST_SPHERE_SHAPE[0] * OUTERMOST_SPHERE_SHAPE[1]
+
 def weight_variable(shape):
     """Generate a tensor of weight variables of dimensions `shape`.
     Initialize them with a small amount of noise for symmetry breaking
@@ -61,30 +63,37 @@ def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-def _add_logit_layer(input):
-    """Build a single softmax layer.
+def _add_dense_linear_layer(input):
+    """Build a single densely connected layer with no activation component
 
     Args:
-      input: The upstream tensor. float - [batch_size, N]
+      input: The upstream tensor. float - [batch_size, dim1, dim2, 1] (last 2 dims optional)
 
     Returns:
-      softmax_linear: Output tensor with the computed logits.
+      logits: Output tensor with the computed logits. - [batch_size, OUTERMOST_SPHERE_N]
     """
 
-    print("_add_logit_layer input:", input)
-
-    with tf.name_scope('softmax_linear'):
-        nRows, nCols = OUTERMOST_SPHERE_SHAPE
-        nOuterCells = nRows*nCols
-        nInnerCells = int(input.get_shape()[1])
+    with tf.name_scope('dense_linear'):
+        input_shape = input.get_shape().as_list()
+        assert (len(input_shape) < 4
+                or len(input_shape) == 4 and input_shape[-1] == 1), 'bad input shape'
+        nInnerCells = 1
+        for dim in input_shape[1:]:
+            nInnerCells *= dim
+        #print('nInnerCells: ', nInnerCells, type(nInnerCells))
+        nOuterCells = OUTERMOST_SPHERE_N
         batch_size = tf.shape(input)[0]
         wtStdv = 1.0 / math.sqrt(float(nOuterCells))
         weights = tf.Variable(tf.truncated_normal([nInnerCells, nOuterCells],
                                                   stddev= wtStdv),
                               name='logit_w')
         biases = tf.Variable(tf.zeros([nOuterCells]), name='logit_b')
-        logits = tf.reshape(tf.matmul(input, weights) + biases,
-                            [batch_size, nRows, nCols])
+        #print('input', input)
+        shaped_input = tf.reshape(input, [batch_size, nInnerCells])
+        #print('shaped input: ', shaped_input)
+        #print('weights: ', weights)
+        #print('biases: ', biases)
+        logits = tf.matmul(shaped_input, weights) + biases
     return logits
 
 
@@ -119,118 +128,185 @@ def _add_cross(inputImg):
     return tf.where(expandedMask, expandedCross, inputImg)
 
 
-def inference(feature, patternStr):
+# def build_convolution( input, kernel_size, stride_size, output_dims):
+#     """
+#     Given input with shape [batch_size, xdim, ydim], build a layer with
+#     the steps:
+#     * convolve with a moving filter of size filter_dims, producing
+#     """
+#     
+#     layer - takes in last layer.
+# kernel - kernel size for convoluting on the image.
+# input_shape - size of the input image.
+# ouput_shape - size of the convoluted image.
+# stride_size - determines kernel jump size
+# def conv_layer(self,layer, kernel, input_shape, output_shape, stride_size):
+#      weights = self.add_weights([kernel, kernel, input_shape,
+#      output_shape])
+#      biases = self.add_biases([output_shape])
+#      #stride=[image_jump,row_jump,column_jump,color_jump]=[1,1,1,1]
+#      stride = [1, stride_size, stride_size, 1]
+#      #does a convolution scan on the given image
+#      layer = tf.nn.conv2d(layer, weights, strides=stride, 
+#              padding='SAME') + biases               
+#      return layer
+
+def build_filter(input, pattern_str):
+    """Build the model up to where it may be converted to a logit.
+
+    Args:
+      input : input tensor; required shape varies by pattern_str
+
+      pattern_str : str - string specifying network pattern. One of:
+        'strip_outer_layer' : [batch_size, N_BALL_SAMPS] -> [batch_size, nRows, nCols, 1]
+        'outer_layer_cnn': [batch_size, nRows, nCols, 1]
+                  
+
+    Returns:
+      output: output tensor; expected shape varies by pattern_str
+
+    """
+
+    # Some convenient constants
+    nRows, nCols = OUTERMOST_SPHERE_SHAPE
+    nOuterCells = nRows*nCols
+
+    if pattern_str == 'strip_outer_layer':
+
+        """ 
+        Strip the outer layer of the ball as an array.
+        The expected input shape is [batch_size, N_BALL_SAMPS].
+        Output shape is [batch_size, nRows, nCols]
+        """
+
+        # The layers of the ball are stored in a 1D array as
+        # [ --layer0-- , --layer1--, ... , --outermost_layer-- ]
+        skinStart = N_BALL_SAMPS - nOuterCells
+
+        # Slice the outer layer of pixels from the feature
+        # outer_skin : [batch_size, nOuterCells]
+        outer_skin = tf.slice(input, [0, skinStart], [-1, nOuterCells])
+
+        # Reshape the outer_skin into 2 dimensions and 1 channel : [nRows, nCols, 1]
+        input_skin = tf.reshape(outer_skin, [-1, nRows, nCols, 1], name="input")
+        
+        #tf.summary.image('input_outer_skin', input_skin, max_outputs=100)
+
+        return input_skin
+    
+    elif pattern_str == 'outer_layer_cnn':
+        """
+        Apply a CNN to the outer layer of the ball.  The expected input shape is [batch_size, N_BALL_SAMPS]
+
+        """
+        assert input.get_shape()[1:] == [nRows, nCols, 1], "wrong input shape %s" % input.get_shape()
+
+        # Convolutional layer #1
+        # conv1 : [batch_size, nRows, nCols, 8]
+        conv1 = tf.contrib.layers.conv2d(
+            inputs=input,
+            num_outputs=8,
+            kernel_size=[5, 5],
+            activation_fn=tf.nn.relu,
+            padding="SAME",
+            scope="conv1")
+        print('conv1: ', conv1)
+
+        # Pooling layer #1
+        # pool1 : [batch_size, ceil(nRows / 2), ceil(nCols / 2), 8]
+        pool1 = tf.contrib.layers.max_pool2d(
+            inputs=conv1,
+            kernel_size=[2, 2],
+            stride=2,
+            padding="SAME",
+            scope="pool1")
+
+        print('pool1:', pool1)
+
+        # Convolutional layer #2
+        # conv2 : [batch_size, ceil(nRows / 2), ceil(nCols / 2), 8]
+        conv2 = tf.contrib.layers.conv2d(
+            inputs=pool1,
+            num_outputs=16,
+            kernel_size=[5, 5],
+            activation_fn=tf.nn.relu,
+            padding="SAME",
+            scope="conv2")
+        print('conv2: ', conv2)
+
+        # Pooling layer #2
+        # pool2 : [batch_size, ceil(nRows / 4), ceil(nCols / 4), 16 ]
+        pool2 = tf.contrib.layers.max_pool2d(
+            inputs=conv2,
+            kernel_size=[2, 2],
+            stride=2,
+            padding="SAME",
+            scope="pool2")
+        print('pool2: ', pool2)
+
+        pool2_dim = pool2.get_shape().as_list()
+        batch_size, pool2_height, pool2_width, pool2_filters = pool2_dim
+
+        num_units = pool2_height * pool2_width * pool2_filters
+
+        # Fully connected relu layer
+        with tf.variable_scope("dense") as scope:
+            num_neurons = nOuterCells
+
+            # Flatten pool2 into [batch_size, h * w * filters]
+            pool2_flat = tf.reshape(pool2, [-1, num_units],
+                                    name="pool2_flat")
+            print('pool2_flat:', pool2_flat)
+
+            weights = weight_variable([num_units, num_neurons])
+            biases  = bias_variable([num_neurons])
+
+            # dense : [batch_size, nOuterCells]
+            dense = tf.nn.relu(tf.matmul(pool2_flat, weights) + biases, name=scope.name)
+
+        return tf.reshape(dense, [-1, nRows, nCols, 1])
+    elif pattern_str == 'dense_linear':
+        return _add_dense_linear_layer(input)
+    else:
+        raise RuntimeError('Unknown inference pattern "%s"' % pattern_str)
+
+
+def inference(feature, pattern_str):
     """Build the model up to where it may be used for inference.
 
     Args:
       feature : [batch_size, N_BALL_SAMPS] - feature placeholder, from inputs().
 
-      patternStr : str - string specifying network pattern. One of:
+      pattern_str : str - string specifying network pattern. One of:
                   'outer_layer_cnn'
 
     Returns:
-      softmax_linear: Output tensor with the computed logits.
+      something_not_softmax_linear: Output tensor with the computed logits.
 
     """
 
-    if patternStr == 'outer_layer_cnn':
+    if pattern_str == 'outer_layer_cnn':
 
         """ Apply a CNN to the outer layer of the ball.
 
         """
 
         with tf.name_scope('cnn'):
-            nRows, nCols = OUTERMOST_SPHERE_SHAPE
-            nOuterCells = nRows*nCols
+            
+            outer_layer = build_filter(feature, 'strip_outer_layer')
 
-            # The layers of the ball are stored in a 1D array as
-            # [ --layer0-- , --layer1--, ... , --outermost_layer-- ]
-            skinStart = N_BALL_SAMPS - nOuterCells
-
-            # Slice the outer layer of pixels from the feature
-            # outer_skin : [batch_size, nOuterCells]
-            outer_skin = tf.slice(feature, [0, skinStart], [-1, nOuterCells])
-
-            # Reshape the outer_skin into 2 dimensions and 1 channel : [nRows, nCols, 1]
-            input_skin = tf.reshape(outer_skin, [-1, nRows, nCols, 1], name="input")
-
-          #  tf.summary.image('input_outer_skin',
-           #                  tf.reshape(input_skin, [-1, nRows, nCols, 1]),
-            #                 max_outputs=100)
-
-            # Convolutional layer #1
-            # conv1 : [batch_size, nRows, nCols, 8]
-            conv1 = tf.contrib.layers.conv2d(
-                inputs=input_skin,
-                num_outputs=8,
-                kernel_size=[5, 5],
-                activation_fn=tf.nn.relu,
-                padding="SAME",
-                scope="conv1")
-
-            # Pooling layer #1
-            # pool1 : [batch_size, ceil(nRows / 2), ceil(nCols / 2), 8]
-            pool1 = tf.contrib.layers.max_pool2d(
-                inputs=conv1,
-                kernel_size=[2, 2],
-                stride=2,
-                padding="SAME",
-                scope="pool1")
-
-            print('pool1:', pool1)
-
-            # Convolutional layer #2
-            # conv2 : [batch_size, ceil(nRows / 2), ceil(nCols / 2), 8]
-            conv2 = tf.contrib.layers.conv2d(
-                inputs=pool1,
-                num_outputs=16,
-                kernel_size=[5, 5],
-                activation_fn=tf.nn.relu,
-                padding="SAME",
-                scope="conv2")
-
-            # Pooling layer #2
-            # pool2 : [batch_size, ceil(nRows / 4), ceil(nCols / 4), 16 ]
-            pool2 = tf.contrib.layers.max_pool2d(
-                inputs=conv2,
-                kernel_size=[2, 2],
-                stride=2,
-                padding="SAME",
-                scope="pool2")
-
-            pool2_dim = pool2.get_shape().as_list()
-            batch_size, pool2_height, pool2_width, pool2_filters = pool2_dim
-
-            num_units = pool2_height * pool2_width * pool2_filters
-
-            # Flatten pool2 into [batch_size, h * w * filters]
-            pool2_flat = tf.reshape(pool2, [-1, num_units],
-                                    name="pool2_flat")
-
-            print('pool2_flat:', pool2_flat)
-
-            # Fully connected relu layer
-            with tf.variable_scope("dense") as scope:
-                num_neurons = nOuterCells
-
-                # Flatten pool2 into [batch_size, h * w * filters]
-                pool2_flat = tf.reshape(pool2, [-1, num_units],
-                                        name="pool2_flat")
-
-                weights = weight_variable([num_units, num_neurons])
-                biases  = bias_variable([num_neurons])
-
-                # dense : [batch_size, nOuterCells]
-                dense = tf.nn.relu(tf.matmul(pool2_flat, weights) + biases, name=scope.name)
-
+            dense = build_filter(outer_layer, pattern_str)
+            print('dense: ', dense)
+            
             # Apply dropout to prevent overfitting
             # dropout = tf.contrib.layers.dropout(
             #     inputs=dense, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
 
-        logits = _add_logit_layer(dense)
+            logits = build_filter(dense, 'dense_linear')
+            print('logits: ', logits)
 
     else:
-        raise RuntimeError('Unknown inference pattern "%s"' % patternStr)
+        raise RuntimeError('Unknown inference pattern "{}"'.format(pattern_str))
 
     return logits
 
