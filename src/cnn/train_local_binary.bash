4298c52e8499430eb4da196edb68f9391d796579
Joel Welling <welling@psc.edu>
2019-07-03 23:53:40 -0400

    Improvements to ability to control partial loading of ckpts

--- src/cnn/train_local_binary.bash
+++ src/cnn/train_local_binary.bash
@@ -4,7 +4,7 @@
 
 cd ${HOME}/git/tensorflow_apps/src/cnn
 python ./train_binaryclassifier.py \
-	--network_pattern outer_layer_cnn_to_binary \
+	--network_pattern outer_layer_logits_to_binary \
 	--batch_size 4 \
 	--data_dir /home/welling/data/fish_cubes \
 	--log_dir ${HOME}/git/tensorflow_apps/log/train_local_log \
@@ -12,5 +12,9 @@ python ./train_binaryclassifier.py \
     --shuffle_size 4 \
 	--num_examples 9 \
 	--file_list /home/welling/data/fish_cubes_train_gp0.txt	\
-	--verbose=True
+	--verbose=False \
+	--check_numerics=True \
+	--starting_snapshot=${HOME}/sshfshook/logs/cnn_train_5951751cnn-4439 \
+	--snapshot_load='cnn' \
+	--hold_constant='cnn'
 	
