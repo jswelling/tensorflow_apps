ed5ed5555684e3788bbb7339484e866d0bcf9741
Joel Welling <welling@psc.edu>
2019-07-15 22:19:05 -0400

    Attempting to further accelerate random rotations

--- src/cnn/rotations.py
+++ src/cnn/rotations.py
@@ -22,10 +22,6 @@ OUTERMOST_SPHERE_SHAPE = [49, 97]  # Number of GLQ samples in outermost shells
 RAD_PIXELS = 20   # radius of outermost shell in pixels
 MAX_L = 48  # maximum L value for outermost shell harmonic expansion
 
-FULL_CHAIN = None
-L_DICT = None
-
-
 def latToTheta(lat):
     """
     Takes latitude values as provided by GLQGridCoord and converts to correctly oriented radians.
@@ -40,24 +36,21 @@ def lonToPhi(lon):
     return (np.pi/180.0) * lon
 
 def prep_rotations(edgeLen, maxL, rMax):
-    global FULL_CHAIN
-    global L_DICT
     fullChain = [(0, 0, 0)]
     for edge in range(1, edgeLen+1):
         r = 0.5 * edge
         l = int(ceil((edge * maxL)/ float(edgeLen + 1)))
         fullChain.append((edge, r, l))
     fullChain.append(((edgeLen+1), rMax, maxL))
-    FULL_CHAIN = fullChain
     
     lDict = {}
     for _, _, l in fullChain:
         nodes, weights = psh.SHGLQ(l)
         lDict[l] = (nodes, weights, psh.djpi2(l))
-    L_DICT = lDict
     print('##### Completed prep_rotations')
+    return fullChain, lDict
 
-def random_rotate_ball_data(vals_to_rotate):
+def random_rotate_ball_data(vals_to_rotate, full_chain, l_dict):
     r0, r1, r2 = np.random.random(size=3)
     theta = np.arccos((2.0 * r0) - 1.0)
     phi = 2.0 * np.pi * r1
@@ -69,31 +62,31 @@ def random_rotate_ball_data(vals_to_rotate):
     rot = Quaternion.fromAxisAngle(vecM, alpha).toTransform()
     theta0, theta1, theta2 = transToEulerRzRyRz(rot)
     thetaArr = np.array([-theta2, -theta1, -theta0])
-    edgeLen = 2 * RAD_PIXELS + 1
-    maxL = MAX_L
-    rMax = 0.5*float(edgeLen + 1)
     
-    if FULL_CHAIN is None:
-        prep_rotations(edgeLen, maxL, rMax)
-        
     sampOffset = 0
     rslt = np.zeros_like(vals_to_rotate)
-    for _, _, l in FULL_CHAIN:
+    for _, _, l in full_chain:
         sampDim1 = l + 1
         sampDim2 = (2 * l) + 1
         sampBlkSz = sampDim1 * sampDim2
-        sampBlk = np.zeros(sampBlkSz)
-        sampBlk[:] = vals_to_rotate[sampOffset: sampOffset+sampBlkSz]
-        nodes, weights, rotMtx = L_DICT[l]
-        hrmBlk = psh.SHExpandGLQ(sampBlk.reshape((sampDim1, sampDim2)), weights, nodes)
-        hrmRotBlk = psh.SHRotateRealCoef(hrmBlk, thetaArr, rotMtx)
-        sampRotBlk = psh.MakeGridGLQ(hrmRotBlk, nodes)
-        rslt[sampOffset: sampOffset + sampBlkSz] = sampRotBlk.flat
+        if l in [MAX_L]:
+            sampBlk = vals_to_rotate[sampOffset: sampOffset+sampBlkSz]
+            nodes, weights, rotMtx = l_dict[l]
+            hrmBlk = psh.SHExpandGLQ(sampBlk.reshape((sampDim1, sampDim2)), weights, nodes)
+            hrmRotBlk = psh.SHRotateRealCoef(hrmBlk, thetaArr, rotMtx)
+            sampRotBlk = psh.MakeGridGLQ(hrmRotBlk, nodes)
+            rslt[sampOffset: sampOffset + sampBlkSz] = sampRotBlk.flat
         sampOffset += sampBlkSz
     return rslt
 
 def random_rotate_op(images):
-    rslt = np.apply_along_axis(random_rotate_ball_data, 1, images)
+
+    edge_len = 2 * RAD_PIXELS + 1
+    r_max = 0.5*float(edge_len + 1)
+    full_chain, l_dict = prep_rotations(edge_len, MAX_L, r_max)
+
+    rslt = np.apply_along_axis(random_rotate_ball_data, 1, images,
+                               full_chain=full_chain, l_dict=l_dict)
     return tf.convert_to_tensor(rslt)
 
 def apply_random_rotation(images, labels):
