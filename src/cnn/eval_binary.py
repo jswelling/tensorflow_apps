1fba86e9d7cda539d980bae264ab71363f6f5f89
Minyue Fan <minyuef@br005.pvt.bridges.psc.edu>
2019-07-17 16:37:58 -0400

    WIP

--- src/cnn/eval_binary.py
+++ src/cnn/eval_binary.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -190,3 +188,4 @@ def main(argv=None):  # pylint: disable=unused-argument
 
 if __name__ == '__main__':
     tf.app.run()
+
