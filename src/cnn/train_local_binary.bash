619d3ddc461379d0cda27aaf61f8ee2b080a0cdd
Joel Welling <welling@psc.edu>
2019-07-18 02:35:59 -0400

    Improvements to the logic for choosing what to restore from checkpoints.

--- src/cnn/train_local_binary.bash
+++ src/cnn/train_local_binary.bash
@@ -6,7 +6,7 @@ export LD_PRELOAD="/usr/lib/libtcmalloc_minimal.so.4"
 
 cd ${HOME}/git/tensorflow_apps/src/cnn
 python ./train_binaryclassifier.py \
-	--network_pattern outer_layer_logits_to_binary \
+	--network_pattern two_layers_logits_to_binary \
 	--batch_size 4 \
 	--data_dir /home/welling/data/fish_cubes \
 	--log_dir ${HOME}/git/tensorflow_apps/log/train_local_log \
@@ -15,12 +15,12 @@ python ./train_binaryclassifier.py \
 	--num_examples 9 \
 	--file_list /home/welling/data/fish_cubes_train_gp0.txt	\
 	--verbose=False \
-	--check_numerics=True \
+	--snapshot_load='cnn,classifier' \
+	--hold_constant='classifier' \
+	--starting_snapshot=${HOME}/sshfshook/logs/cnn_train_6045675cnn-4404 \
 	--reset_global_step \
 	--random_rotation
-	
+
 	#--starting_snapshot=${HOME}/git/tensorflow_apps/log/train_local_logcnn-7 \
-	#--snapshot_load='cnn' \
-	#--hold_constant='cnn' \
-	#--starting_snapshot=${HOME}/sshfshook/logs/cnn_train_5951751cnn-4439 \
-		
+	#--check_numerics=True \
+								
